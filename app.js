33e987e6b93bb56f7471d9d9f106102ffe7ecd40
Olivier02500 <olivier.peuchet@hotmail.com>
2021-11-13 12:35:09 +0100

    make exo 53

---
+++ app.js
@@ -0,0 +1,142 @@
+
+/*
+ *   modify P
+ */
+
+let para1 = document.getElementById('para1');
+para1.style.color = 'blue';
+para1.style.width = '80%';
+console.log(para1);
+
+let para2 = document.getElementById('para2');
+para2.style.color = 'orangered';
+para2.style.width = '80%';
+console.log(para2);
+
+let para3 = document.getElementById('para3');
+para3.style.color = 'red';
+para3.style.width = '80%';
+console.log(para3);
+
+
+let para4 = document.getElementById('para4');
+para4.style.color = 'orange';
+para4.style.width = '80%';
+console.log(para4);
+
+
+let para5 = document.getElementById('para5');
+para5.style.color = 'green';
+para5.style.width = '80%';
+para5.innerText = "Modifiez le texte du dernier paragraphe"
+console.log(para5);
+
+/*
+* modify span
+*/
+
+let spa1_1 = document.getElementById('spa1_1');
+spa1_1.style.backgroundColor = 'orangered';
+spa1_1.style.direction = 'rtl';
+console.log(spa1_1);
+
+let spa1_2 = document.getElementById('spa1_2');
+spa1_2.style.backgroundColor = 'orangered';
+spa1_2.style.direction = 'rtl';
+console.log(spa1_2);
+
+let spa2_1 = document.getElementById('spa2_1');
+spa2_1.style.backgroundColor = 'green';
+spa2_1.style.direction = 'rtl';
+console.log(spa2_1);
+
+let spa2_2 = document.getElementById('spa2_2');
+spa2_2.style.backgroundColor = 'green';
+spa2_2.style.direction = 'rtl';
+console.log(spa2_2);
+
+let spa3_1 = document.getElementById('spa3_1');
+spa3_1.style.backgroundColor = 'blue';
+spa3_1.style.direction = 'rtl';
+console.log(spa3_1);
+
+let spa3_2 = document.getElementById('spa3_2');
+spa3_2.style.backgroundColor = 'blue';
+spa3_2.style.direction = 'rtl';
+console.log(spa3_2);
+
+let spa4_1 = document.getElementById('spa4_1');
+spa4_1.style.backgroundColor = 'red';
+spa4_1.style.direction = 'rtl';
+console.log(spa4_1);
+
+let spa4_2 = document.getElementById('spa4_2');
+spa4_2.style.backgroundColor = 'red';
+spa4_2.style.direction = 'rtl';
+console.log(spa4_2);
+
+let spa5_1 = document.getElementById('spa5_1');
+spa5_1.style.backgroundColor = 'orangered';
+spa5_1.style.direction = 'rtl';
+console.log(spa5_1);
+
+let spa5_2 = document.getElementById('spa5_2');
+spa5_2.style.backgroundColor = 'orangered';
+spa5_2.style.direction = 'rtl';
+console.log(spa5_2);
+
+/*
+*   animation
+ */
+
+/*
+    P2
+ */
+
+let sizeName = document.getElementById('para3')
+sizeName.style.animationName= 'size';
+
+let size = document.getElementById('para3')
+size.style.animationIterationCount = '2';
+
+let sizeTime = document.getElementById('para3')
+sizeTime.style.animationDuration = '5s'
+
+let sizeFont = document.getElementById('para3')
+sizeFont.style.fontSize = '1.8rem'
+
+/*
+    P3
+ */
+
+let sizeName2 = document.getElementById('para5')
+sizeName2.style.animationName= 'size';
+
+let size2 = document.getElementById('para5')
+size2.style.animationIterationCount = '2';
+
+let sizeTime2 = document.getElementById('para5')
+sizeTime2.style.animationDuration = '5s'
+
+let sizeFont2 = document.getElementById('para5')
+sizeFont2.style.fontSize = '1.8rem'
+
+/*
+    modify A
+ */
+
+let addLien1 = document.getElementById('lien1')
+addLien1.innerText = "lorem ipsum"
+
+let addLien2 = document.getElementById('lien2')
+addLien2.innerText = "lorem ipsum"
+
+let addLien3 = document.getElementById('lien3')
+addLien3.innerText = "lorem ipsum"
+
+let addLien4 = document.getElementById('lien4')
+addLien4.innerText = "lorem ipsum"
+
+let addLien5 = document.getElementById('lien5')
+addLien5.innerText = "lorem ipsum"
+
